73da7ac05b962cd9b713b328fd6aebe6ad37f029
bm rifat <bmrifat93@gmail.com>
2025-11-24 14:59:31 +0600

    js section

---
+++ javaScript/main.js
@@ -0,0 +1,8 @@
+const categoryContainer = document.getElementById("category-container");
+const postContainer = document.getElementById("post-container");
+const loader = document.getElementById("loader");
+const modalContainer = document.getElementById("modal-container");
+const cartContainer = document.getElementById("cart-container");
+const totalPrice = document.getElementById("total-price");
+
+let addToCart = [];
